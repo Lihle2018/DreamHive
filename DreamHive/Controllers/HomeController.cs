5e5d744fd6fbd233ffb91c1594f51c6a75c354ba
Lihle Mqhayi <2018293130@ufs4life.ac.za>
2022-12-18 11:12:30 +0200

    Dream Hive

---
+++ DreamHive/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using DreamHive.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DreamHive.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        [AutoValidateAntiforgeryToken]
+        public IActionResult Index(Contact contact)
+        {
+            if(ModelState.IsValid )
+            {
+                return View();
+            }
+            return View(contact);
+        }
+    }
+}
---
+++ DreamHive/Models/Contact.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DreamHive.Models
+{
+    public class Contact
+    {
+        [Required(ErrorMessage ="Please enter your name")]
+        public string? Name { get; set; }
+        [Required(ErrorMessage ="Please enter you email")]
+        [RegularExpression(@"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}" + @"\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\" + @".)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$",ErrorMessage = "Email is not valid")]
+        public string? Email { get; set; }
+        [Required(ErrorMessage ="Please enter the subject")]
+        public string? Subject { get; set; }
+        [Required(ErrorMessage ="Please write you message")]
+        [MinLength(10,ErrorMessage ="Minimum characters is 10")]
+        [MaxLength(1000,ErrorMessage ="Your message must not exceed 1000 characters")]
+        public string? Message { get; set; }
+    }
+}
